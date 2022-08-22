cd2784844fd38b0de764a2c03cbda1efa03c7873
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-08-22 12:48:50 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (num1,num2) => num1 / num2
+    divide(2000,100)
+    console.log (divide(2000,100));
+   
+const square = (num1) => num1 **2
+     square(2)
+     console.log(square(2));
+     
+const add = (a,b) => a + b
+      add(3,4)
+      console.log((add(3,4)));   
